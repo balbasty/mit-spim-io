3b9e3be4da0dcda4fa5dbfa88ebb5036dded2f77
Yael Balbastre <yael.balbastre@gmail.com>
2021-08-09 11:55:22 -0400

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='mit-spim-io',
+    version='',
+    packages=['spimio'],
+    url='https://github.com/balbasty/mit-spim-io',
+    license='',
+    author='Yael Balbastre',
+    author_email='ybalbastre@mgh.harvard.edu',
+    description='Tiny library to read/stream SPIM data stored on DANDI'
+)
